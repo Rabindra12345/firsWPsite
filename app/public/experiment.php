1b53f56b825d4d3a8a2f27166f6065494bd13761
rabindra <rabindra@red27mobile.com>
2023-04-01 16:28:16 +0545

    adding an experiment file for test

---
+++ app/public/experiment.php
@@ -0,0 +1,13 @@
+<b>WHAT THE Hell MAN , JUST GOT IT DONE . THE FUCK !!</b>
+<br><br>
+<?php  echo 2+765674?>
+
+<?php echo 
+    $name = 'rABINDRA..'
+?>
+
+<h1>THIS PAGE IS MADE BY ONE N ONLY <?php echo $name?></h1>
+
+
+<h2>HELLO , WELCOME WELCOME ....WELCOME  <?php echo $name?></h2>
+
